a9efeef497f80d25954ada44253d6119e3be5141
Justraymond5 <collegeeduraymond@gmail.com>
2025-01-15 19:58:56 -0800

    Add project files.

---
+++ COMP003A.LectureActivity1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//namespace level
+namespace COMP003A.LectureActivity1
+{
+    //class level
+    internal class Program
+    {
+        //mail level
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
